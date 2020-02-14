5d98b62e5c3fbb2713f9444728cc9a0669b8cfe9
Joao Guimaraes <jssgmrs22@gmail.com>
2020-02-14 15:41:07 +0000

    Initial Commit

---
+++ src/components/landingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Particles from "react-particles-js";
+
+const LandingPage = () => {
+  return (
+    <div className="LandingPage">
+      <Particles class="particles" params={pconfig} />
+    </div>
+  );
+};
+
+export default LandingPage;
